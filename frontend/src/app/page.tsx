a91625ed1ba5e3f43dbd7e65c4d391055688c230
greizs <brandelreyno@gmail.com>
2026-05-01 13:56:42 -0600

    feat: Configuración del frontend en Next.js con Typescript, Tailwind y Axios

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <h1 className="text-3xl font-bold text-center">
+        API Explorer 🚀
+      </h1>
+    </main>
+  );
+}
